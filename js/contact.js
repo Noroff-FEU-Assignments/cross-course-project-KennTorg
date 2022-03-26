e6ac9aa99fe8ef962082143cba8976334ec8b6ee
KennTorg <91532197+KennTorg@users.noreply.github.com>
2022-03-26 19:38:33 +0100

    close

---
+++ js/contact.js
@@ -0,0 +1,51 @@
+const form = document.querySelector("#contactForm");
+const name = document.querySelector("#name");
+const nameError = document.querySelector("#nameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#messageError");
+
+function validateForm() {
+    event.preventDefault();
+
+    if (checkLength(name.value, 0) === true) {
+        nameError.style.display = "none";
+    } else {
+        nameError.style.display = "block";
+    }
+
+    if (checkLength(name.value, 0) === true) {
+        nameError.style.display = "none";
+    } else {
+        nameError.style.display = "block";
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+
+    if (checkLength(message.value, 9) === true) {
+        messageError.style.display = "none";
+    } else {
+        messageError.style.display = "block";
+    }
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S\.\S+/;
+    const patternMatch = regEx.test(email);
+    return patternMatch;
+}
